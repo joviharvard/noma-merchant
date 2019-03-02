1ca1df395f665453b1c2a70013065fd3d1268604
docovarr2020 <diego_covarrubias@college.harvard.edu>
2019-03-02 15:56:24 -0500

    Config(Apollo Client) -- Refactor client into its own file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react'
-import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import theme from './theme'
-import config from './config'
+import client from './client'
 import Home from './containers/Home/'
-
-// creates new client
-const client = new ApolloClient({
-  uri: config.graphqlUrl
-})
 
 class App extends Component {
   render() {
